6c8d3a76e563802d896bb0704d77275cb0e8d044
rusty-bytes7 <147008821+rusty-bytes7@users.noreply.github.com>
2024-11-25 11:30:06 -0500

    some more fixes

--- machinelearningmodule14.py
+++ machinelearningmodule14.py
@@ -3,8 +3,19 @@ import pandas as pd
 from sklearn import tree
 import matplotlib.pyplot as plot
 
+clf = tree.DecisionTreeClassifier()
+
 test = pd.read_csv('/Users/ericawolf/vscode/testdigits.csv')
 training = pd.read_csv('/Users/ericawolf/vscode/trainingdigits.csv')
 
 print('The length of the test data is:',len(test))
 print('The length of the training data is:', len(training))
+
+#assign data to numpy array
+testdata = test.to_numpy()
+trainingdata = training.to_numpy()
+
+#assign images to variable
+number = testdata[0:1, 1:]
+number.shape = (28,28)
+plot.imshow(number,cmap = 'gray')
